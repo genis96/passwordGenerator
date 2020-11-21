4b5b78177b7a896f8a57a16a858709831a50fd16
Genis Bardales <genis96@yahoo.com>
2020-11-20 19:37:39 -0500

    added pseudo

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,11 @@ let generateBtn = document.querySelector("#generate");
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
+
+//generatePassword - will check upper/lower/special char. 
+function generatePassword() {
 
 }
 
@@ -16,3 +19,18 @@ generateBtn.addEventListener('click', writePassword);
 
 
 
+/* Pseudo+
++ create series of prompts once clicked. 
+ - prompt that takes in length of character b/w 8-128
+ - prompt that takes specific character types which must have an uppercase, lowercase and/or special characters 
+ - when answered, it validates 
+
+
+ -click generate password btn 
+ - call function to generate passwords 
+ - cycle through the criteria 
+ -return password from the generatePassword function
+ -add the password to the screen 
+
+
+*/
