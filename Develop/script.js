8ce0912ce8b3aba9357bd73fd29cea7f5aa85620
Genis Bardales <genis96@yahoo.com>
2020-11-21 20:19:33 -0500

    removed notes

--- Develop/script.js
+++ Develop/script.js
@@ -34,18 +34,3 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-/* Pseudo+
-+ create series of prompts once clicked. 
- - prompt that takes in length of character b/w 8-128
- - prompt that takes specific character types which must have an uppercase, lowercase and/or special characters 
- - when answered, it validates 
-
-
- -click generate password btn 
- - call function to generate passwords 
- - cycle through the criteria 
- -return password from the generatePassword function
- -add the password to the screen 
-
-
-*/
